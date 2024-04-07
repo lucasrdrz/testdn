c6624657ca74ffe711b0ea577265c5ab13823a86
Lucas Rodriguez <110471982+lucasrdrz@users.noreply.github.com>
2024-04-07 07:04:12 -0300

    Update streamlitTEST.py

--- pages/streamlitTEST.py
+++ pages/streamlitTEST.py
@@ -5,7 +5,7 @@ df = pd.read_excel("intento70-30-05-04.xlsx")
 
 pdescripciones=pd.read_excel("FormatoNsPartes.xlsx")
 
-daily = pd.read_excel("C://Users//lucas.rodriguez//OneDrive - Diebold Nixdorf//Escritorio//Codigo//PARTES//daily.xlsx")
+daily = pd.read_excel("daily.xlsx")
 
 # Combinar los dataframes en función de la columna "PARTES" y "PARTE"
 merged_df = pd.merge(df, pdescripciones[['PARTE', 'DESCRIPCION']], how='left', left_on='PARTES', right_on='PARTE')
