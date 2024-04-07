599a326cdb8b9b0157a0e5429adcc131731aa572
Lucas Rodriguez <110471982+lucasrdrz@users.noreply.github.com>
2024-04-07 07:06:56 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,6 +8,7 @@ df = pd.read_excel("intento70-30-05-04.xlsx")
 
 pdescripciones=pd.read_excel("FormatoNsPartes.xlsx")
 
+daily = pd.read_excel("Daily.xlsx")
 
 # Combinar los dataframes en función de la columna "PARTES" y "PARTE"
 merged_df = pd.merge(df, pdescripciones[['PARTE', 'DESCRIPCION']], how='left', left_on='PARTES', right_on='PARTE')
@@ -18,9 +19,6 @@ merged_df.rename(columns={'DESCRIPCION': 'descripciones'}, inplace=True)
 # Eliminar la columna 'PARTE' redundante
 merged_df.drop(columns=['PARTE'], inplace=True)
 
-# Guardar el resultado si es necesario
-# merged_df.to_excel('resultado.xlsx', index=False)
-
 # Agrupar por número de parte y contar tickets únicos
 tickets_por_parte = df.groupby('PARTES')['TICKET NUMBER'].nunique().reset_index()
 
@@ -30,16 +28,32 @@ tickets_por_parte.rename(columns={'TICKET NUMBER': 'TICKETS AFECTADOS'}, inplace
 # Fusionar con pdescripciones para obtener descripciones
 tickets_por_parte = pd.merge(tickets_por_parte, pdescripciones[['PARTE', 'DESCRIPCION']], how='left', left_on='PARTES', right_on='PARTE')
 
-# Seleccionar las columnas requeridas
-resultado_final = tickets_por_parte[['PARTES', 'DESCRIPCION', 'TICKETS AFECTADOS']]
+# Filtrar registros con más de 10 tickets afectados
+resultado_final = tickets_por_parte[tickets_por_parte['TICKETS AFECTADOS'] >= 10]
 
 # Ordenar los resultados de mayor a menor según el número de tickets afectados
 resultado_final = resultado_final.sort_values(by='TICKETS AFECTADOS', ascending=False)
 
+# Seleccionar las columnas requeridas y cambiar el orden
+resultado_final = resultado_final[['PARTES', 'DESCRIPCION', 'TICKETS AFECTADOS']]
 
+# Ordenar los resultados de mayor a menor según el número de tickets afectados
+resultado_final = resultado_final.sort_values(by='TICKETS AFECTADOS', ascending=False)
+# Mostrar el resultado
 
-st.markdown ('              PARTES SOLICITADAS')
-
-
-# Mostrar los resultados en Streamlit
 st.table(resultado_final)
+
+# Filtrar las columnas deseadas
+columnas_deseadas = ['ESTATUS', 'VENDOR', 'ITEM', 'DESCRIPTION', 'QTY', 'REAL SHIPPING DATE', 'CUSTOMS ARRIVAL', 'ETA WAREHOUSE', 'REAL DELIVERY DATE']
+resultado_filtrado = daily[columnas_deseadas]
+
+# Crear el menú desplegable para seleccionar una parte específica
+parte_seleccionada = st.selectbox("Selecciona una parte:", [""] + resultado_final['PARTES'].tolist())
+
+# Filtrar los resultados según la parte seleccionada
+if parte_seleccionada:
+    resultado_filtrado = resultado_filtrado[resultado_filtrado['ITEM'] == parte_seleccionada]
+    st.write("Información para la parte seleccionada:")
+    st.table(resultado_filtrado)
+else:
+    st.write("Por favor, selecciona una parte para ver la información.")
