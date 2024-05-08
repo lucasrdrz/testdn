9c2f85c7c72cc6fbe27e0dad40876c0d708a3c21
Lucas Rodriguez <110471982+lucasrdrz@users.noreply.github.com>
2024-05-08 16:07:33 -0300

    Update Clientes vs partes.py

--- pages/Clientes vs partes.py
+++ pages/Clientes vs partes.py
@@ -5,7 +5,7 @@ df = pd.read_excel("intento70-30.xlsx")
 
 pdescripciones=pd.read_excel("FormatoNsPartes.xlsx")
 
-
+st.markdown ('**Impacto del Item en Clientes**')
 # Combinar los dataframes en función de la columna "PARTES" y "PARTE"
 merged_df = pd.merge(df, pdescripciones[['PARTE', 'DESCRIPCION']], how='left', left_on='PARTES', right_on='PARTE')
 
