414cee2d5ef840fbda6ffca51a827b7ec5dfb326
Lucas Rodriguez <110471982+lucasrdrz@users.noreply.github.com>
2024-05-31 13:46:31 -0300

    Update streamlit.py
    se agrega banner

--- streamlit.py
+++ streamlit.py
@@ -4,8 +4,15 @@ import pip
 import datetime
 import pytz
 from PIL import Image
+
 image = Image.open("./images/logo1.png")
 st.sidebar.image(image,width=150,use_column_width=False)
+image1 = Image.open("./images/Partes-Criticas.jpg")
+st.image(image1, caption=None, width=750, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+
+
+
 # Obtener la fecha y hora actual en UTC
 fecha_hora_actual_utc = datetime.datetime.now(pytz.utc)
 
